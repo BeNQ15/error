407251e8face2711272d0bd9f8b2037a92b9e9e5
BeNQ15 <118977654+BeNQ15@users.noreply.github.com>
2025-02-25 20:24:47 +0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -2,7 +2,6 @@ const errorButton = document.getElementById('errorButton');
 const errorMessage = document.getElementById('errorMessage');
 const backgroundSound = document.getElementById('backgroundSound');
 const toggleSoundButton = document.getElementById('toggleSoundButton');
-const streamSource = document.getElementById('streamSource');
 
 const errors = [
     "Ошибка 404: Страница не найдена.",
@@ -15,37 +14,18 @@ const errors = [
     "Ошибка 503: Сервис недоступен."
 ];
 
-// Функция для получения URL радиостанции из базы данных
-async function loadRadioStation() {
-    try {
-        const response = await fetch('/api/get_station'); // URL вашего API для получения станции
-        const data = await response.json();
-        if (data.url) {
-            streamSource.src = data.url; // Устанавливаем URL радиостанции
-            backgroundSound.load(); // Перезагружаем аудио элемент
-        } else {
-            console.error('URL радиостанции не найден.');
-        }
-    } catch (error) {
-        console.error('Ошибка при загрузке радиостанции:', error);
-    }
-}
-
-// Загружаем радиостанцию при инициализации
-loadRadioStation();
-
 errorButton.addEventListener('click', () => {
     const randomIndex = Math.floor(Math.random() * errors.length);
     errorMessage.textContent = errors[randomIndex];
 });
 
-// Обработчик для кнопки включения/выключения радиостанции
+// Обработчик для кнопки включения/выключения звука
 toggleSoundButton.addEventListener('click', () => {
     if (backgroundSound.paused) {
         backgroundSound.play();
-        toggleSoundButton.textContent = 'Стоп'; // Меняем текст на "Стоп"
+        toggleSoundButton.textContent = 'Выключить звук';
     } else {
         backgroundSound.pause();
-        toggleSoundButton.textContent = 'Включить'; // Меняем текст на "Включить"
+        toggleSoundButton.textContent = 'Включить звук';
     }
 });
